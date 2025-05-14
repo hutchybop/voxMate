2f02b651632c6c16caf8765ed5f6c6544a97ec5e
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2025-05-14 14:00:00 +0100

    Recording script developed

---
+++ module_testing/recording/rec_test.py
@@ -0,0 +1,18 @@
+# Importing required libraries
+import sounddevice as sd
+import wavio as wv
+
+# Setting the audio frequency
+freq = 16000
+
+# Setting the record duration (seconds)
+duration = 5
+
+# Setting the parameters of the recorder.
+# channels=1 is mono and =2 is sterio
+# Will create a NumPy output, which will be converted below
+recording = sd.rec(int(duration * freq), samplerate=freq, channels=1)
+sd.wait()
+
+# Convert the NumPy array to audio file
+wv.write("recording.wav", recording, freq, sampwidth=2)

eaed01839025d66cbe7d3e9ae378fd3ba80ab66e
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2025-06-28 16:58:53 +0100

    Fix typo in start_voxMate.sh

--- start_voxMate.sh
+++ start_voxMate.sh
@@ -5,7 +5,7 @@ tmux kill-session -t voxMate 2>/dev/null
 tmux new-session -d -s voxMate "bash --login"
 tmux send-keys -t voxMate "source .voxenv/bin/activate" C-m
 tmux send-keys -t voxMate "cd voxMate_web_app" C-m
-tmux send-keys -t voxMate "flask run --host=0.0.0.0 --port=5000 --debuge" C-m
+tmux send-keys -t voxMate "flask run --host=0.0.0.0 --port=5000 --debug" C-m
 echo "voxMate Webapp deployed, 192.168.1.30:5000"
 
 source .voxenv/bin/activate

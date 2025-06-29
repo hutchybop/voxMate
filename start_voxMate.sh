bf54ade09ef08f223d185bd5be4daeed9c3f487e
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2025-06-29 21:10:04 +0100

    Update start and stop scripts

--- start_voxMate.sh
+++ start_voxMate.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Update git to latest version
 git pull
+
+# Setup .voxenv
+source ~/voxMate/.voxenv/bin/activate
+
+# Start the voxMate_web_app
 tmux kill-session -t voxMate 2>/dev/null
 tmux new-session -d -s voxMate "bash --login"
-tmux send-keys -t voxMate "source .voxenv/bin/activate" C-m
-tmux send-keys -t voxMate "cd voxMate_web_app" C-m
+tmux send-keys -t voxMate "source ~/voxMate/.voxenv/bin/activate" C-m
+tmux send-keys -t voxMate "cd ~/voxMate/voxMate_web_app" C-m
 tmux send-keys -t voxMate "flask run --host=0.0.0.0 --port=5000 --debug" C-m
 echo "voxMate Webapp deployed, 192.168.1.30:5000"
 
-source .voxenv/bin/activate
-python3 voxMate.py
+# Run voxMate app
+cd ~/voxMate
+python3 -m voxMate_app.main

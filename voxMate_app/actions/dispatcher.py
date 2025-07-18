1c6f01213e73eabcb5b639dbe7952ab87d0b81b4
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2025-07-18 18:06:08 +0100

    Add function to wait for free device

--- voxMate_app/actions/dispatcher.py
+++ voxMate_app/actions/dispatcher.py
@@ -1,8 +1,25 @@
+# Required python imports
+import sounddevice as sd
+import time
+
+# Required local imports
 from actions.handlers.spotify_app import SpotifyPlayer
 from utils.state import app_state
+from utils.logging import logger
+
 
 # Initisalse SportifyPlayer
 spotify_player = SpotifyPlayer()
+
+def wait_for_device_release(retries=5, delay=0.2):
+    for _ in range(retries):
+        try:
+            sd.query_devices(kind='output')
+            return True
+        except Exception as e:
+            logger.warning(f"Audio device not ready yet: {e}")
+            time.sleep(delay)
+    return False
 
 def handle_cmd(cmd):
     """Handles user commands"""
@@ -24,6 +41,7 @@ def handle_cmd(cmd):
         if not stop:
             message = "Error stopping Spotify"
         else:
+            wait_for_device_release()
             app_state.set_state("spotify", "stopped")
         return stop, message
 
